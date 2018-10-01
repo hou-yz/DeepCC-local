309c828d7db90112f40d1b5a9aaa6f0503f7a3d6
Yunzhong Hou <hou_yz@outlook.com>
2018-10-01 20:20:52 +0800

    Sync: rm triplet-reid

--- src/triplet-reid/heads/__init__.py
+++
@@ -1,7 +0,0 @@
-# Used for the commandline flags.
-HEAD_CHOICES = (
-    'direct',
-    'direct_normalize',
-    'fc1024',
-    'fc1024_normalize',
-)
--- src/triplet-reid/nets/__init__.py
+++
@@ -1,6 +0,0 @@
-# Used for the commandline flags.
-NET_CHOICES = (
-    'mobilenet_v1_1_224',
-    'resnet_v1_50',
-    'resnet_v1_101',
-)
