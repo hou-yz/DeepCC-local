fdc7a4f28f20ed6850475f8da39dffeff88f4a3d
Yunzhong Hou <hou_yz@outlook.com>
2018-10-08 17:01:14 +0800

    Updates: gen_ALL_gt_bbox

--- src/gen_gt_function.m
+++ src/gen_gt_function.m
@@ -1,18 +1,12 @@
 function gen_gt_function(opts,iCam,fps)
 
-sequence_window   = opts.sequence_intervals{opts.sequence};
-start_frame       = global2local(opts.start_frames(iCam), sequence_window(1));
-end_frame         = global2local(opts.start_frames(iCam), sequence_window(end));
+sequence_window   = global2local(opts.start_frames(iCam),opts.sequence_intervals{opts.sequence});
 
 ground_truth = load(fullfile(opts.dataset_path,'ground_truth/trainval.mat'));
 ground_truth = ground_truth.trainData;
-ground_truth = ground_truth(ground_truth(:,1)==iCam & ground_truth(:,3)>= start_frame & ground_truth(:,3)<= end_frame,:);
+ground_truth = ground_truth(ground_truth(:,1)==iCam & ismember(ground_truth(:,3),sequence_window),:);
 
-video_name = fullfile(opts.dataset_path, 'videos', sprintf('camera%d.mp4', iCam));
-videoObject = VideoReader(video_name);
-videoObject.CurrentTime = (start_frame-1) / videoObject.FrameRate;
-
-folder_dir = fullfile(opts.dataset_path, 'ALL_gt_bbox', sprintf('gt_bbox_%d_fps',fps));
+folder_dir = fullfile(opts.dataset_path, 'ALL_gt_bbox',opts.sequence_names{opts.sequence}, sprintf('gt_bbox_%d_fps',fps));
 if exist(folder_dir,'dir') == 0
     mkdir(folder_dir);
 end
@@ -20,12 +14,9 @@ if exist(fullfile(folder_dir,sprintf('camera%d',iCam)),'dir') == 0
     mkdir(fullfile(folder_dir,sprintf('camera%d',iCam)));
 end
 
-frame = start_frame;
-while frame < end_frame 
-    for i = 1:60/fps
-        image = readFrame(videoObject);
-        frame = frame+1;
-    end
+for i = 1:round(60/fps):length(sequence_window)
+    frame = sequence_window(i);
+    image = opts.reader.getFrame(iCam,frame);
     gt_ids = find(ground_truth(:,3) == frame);
     if isempty(gt_ids)
         continue;
--- src/get_ALL_gt_bbox.m
+++ src/get_ALL_gt_bbox.m
@@ -3,10 +3,9 @@ clear;
 close all; 
 opts = get_opts();
 opts.sequence=5;
-opts.reader = [];
 
 % fps at 1/30/60
-fps = 60;
+fps = 1;
 for iCam = 1:8
     gen_gt_function(opts,iCam,fps);
 end
