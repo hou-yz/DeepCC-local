784fac7c1bbd08213ced9bab49225ffe753f095b
hou_yz <hou_yz@outlook.com>
2018-10-18 13:19:21 +0800

    Updates: L1loss, {0,1}target

--- src/hyper_score/main.py
+++ src/hyper_score/main.py
@@ -6,103 +6,128 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from dataset import *
-from sampler import RandomIdentitySampler
+from sampler import *
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, num_class=0):
         super(Net, self).__init__()
+        self.num_class = num_class
         self.fc1 = nn.Linear(256 + 6, 1024)
         self.fc2 = nn.Linear(1024, 1024)
-        self.fc3 = nn.Linear(1024, 1)
+        self.fc3 = nn.Linear(1024, 128)
+        if self.num_class > 0:
+            self.out_layer = nn.Linear(128, self.num_class)
 
-    def forward(self, x1, x2):
-        out = x1 - x2
-        out = self.fc1(out)
+    def forward(self, x):
+        out = self.fc1(x)
         out = F.relu(out)
         out = self.fc2(out)
         out = F.relu(out)
         out = self.fc3(out)
-
+        out = F.relu(out)
+        if self.num_class > 0:
+            out = self.out_layer(out)
         return out
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, train_loader, optimizer, epoch, criterion):
     model.train()
-    for batch_idx, (feat, pid, spaGrpID) in enumerate(train_loader):
-        data, target = feat.to(device), pid.to(device)
+    for batch_idx in range(train_loader.dataset.num_spatialGroup):
+        for _, (feat, pid, spaGrpID) in enumerate(train_loader):
+            l = pid.shape[0]
+            data, target = feat.cuda(), pid.cuda()
+            data = data.unsqueeze(0).expand(l, l, 262) - data.unsqueeze(1).expand(l, l, 262)
+            target = (target.unsqueeze(0).expand(l, l) - target.unsqueeze(1).expand(l, l)) == 0
+            data, target = data.view(-1, 262).float(), target.view(-1, 1).float()
 
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
+
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
+            print('Train Epoch: {}, Batch:{},\tLoss: {:.6f}'.format(
+                epoch, batch_idx, loss.item()))
 
 
-def test(args, model, device, test_loader):
+def test(args, model, test_loader, criterion):
     model.eval()
     test_loss = 0
     correct = 0
+    miss = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
+        for epoch in range(1, test_loader.dataset.num_spatialGroup + 1):
+            for (feat, pid, spaGrpID) in test_loader:
+                l = pid.shape[0]
+                data, target = feat.cuda(), pid.cuda()
+                data = data.unsqueeze(0).expand(l, l, 262) - data.unsqueeze(1).expand(l, l, 262)
+                target = (target.unsqueeze(0).expand(l, l) - target.unsqueeze(1).expand(l, l)) == 0
+                data, target = data.view(-1, 262).float(), target.view(-1, 1).float()
+                output = model(data)
+                test_loss += criterion(output, target).item()  # sum up batch loss
+                pred = output > 0.5  # get the index of the max log-probability
+                correct += pred.eq(target.view_as(pred).byte()).sum().item()
+                miss += target.shape[0] - pred.eq(target.view_as(pred).byte()).sum().item()
+                pass
+            test_loss /= epoch
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        test_loss, correct, correct + miss, 100. * correct / (correct + miss)))
 
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=256, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--num-instances', type=int, default=4,
+    parser.add_argument('--test-batch-size', type=int, default=256, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--num-instances', type=int, default=16,
                         help="each minibatch consist of "
                              "(batch_size // num_instances) identities, and "
                              "each identity has num_instances instances, "
                              "default: 4")
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
                         help='learning rate (default: 0.01)')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                         help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
+    parser.add_argument('--train', action='store_true')
+    # parser.add_argument('--resume', type=str, default='', metavar='PATH')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+    parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                         help='how many batches to wait before logging training status')
     args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
 
-    device = torch.device("cuda" if use_cuda else "cpu")
+    dataset = HyperFeat('../data')
+    train_loader = DataLoader(dataset, batch_size=args.batch_size,
+                              sampler=RandomIdentitySampler(dataset, args.num_instances),
+                              num_workers=0, pin_memory=True)
 
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    dateset = HyperFeat('../data')
-    train_loader = DataLoader(dateset, batch_size=args.batch_size,
-                              sampler=RandomIdentitySampler(dateset, args.num_instances),
-                              **kwargs)
+    test_loader = DataLoader(dataset, batch_size=args.test_batch_size,
+                             sampler=RandomIdentitySampler(dataset, 100),
+                             num_workers=0, pin_memory=True)
 
-    model = Net().to(device)
+    model = Net(num_class=1)
+    model = nn.DataParallel(model).cuda()
+    criterion = nn.L1Loss().cuda()
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+    if args.train:
+        for epoch in range(1, args.epochs + 1):
+            train(args, model, train_loader, optimizer, epoch, criterion)
+            # test(args, model, test_loader, criterion)
+        torch.save({'state_dict': model.module.state_dict(), }, 'checkpoint.pth.tar')
+
+    checkpoint = torch.load('checkpoint.pth.tar')
+    model_dict = checkpoint['state_dict']
+    model.module.load_state_dict(model_dict)
+    test(args, model, test_loader, criterion)
 
 
 if __name__ == '__main__':
