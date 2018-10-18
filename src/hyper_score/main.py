28ff7c79060a3e0f3620d3696bcfdd572229510e
hou_yz <hou_yz@outlook.com>
2018-10-18 21:26:12 +0800

    Updates: add save_result function

--- src/hyper_score/main.py
+++ src/hyper_score/main.py
@@ -36,11 +36,11 @@ def train(args, model, train_loader, optimizer, epoch, criterion):
     for batch_idx in range(train_loader.dataset.num_spatialGroup):
         for _, (feat, pid, spaGrpID) in enumerate(train_loader):
             l = pid.shape[0]
+            spaGrpID = np.unique(spaGrpID)
             data, target = feat.cuda(), pid.cuda()
             data = data.unsqueeze(0).expand(l, l, 262) - data.unsqueeze(1).expand(l, l, 262)
             target = (target.unsqueeze(0).expand(l, l) - target.unsqueeze(1).expand(l, l)) == 0
             data, target = data.view(-1, 262).float(), target.view(-1, 1).float()
-
 
             optimizer.zero_grad()
             output = model(data)
@@ -57,23 +57,37 @@ def test(args, model, test_loader, criterion):
     test_loss = 0
     correct = 0
     miss = 0
+    lines = np.array([]).reshape(0, 2)
+    if args.save_result:
+        iCam = int(args.data_dir[-4])
+    else:
+        iCam = 0
     with torch.no_grad():
-        for epoch in range(1, test_loader.dataset.num_spatialGroup + 1):
+        for batch_idx in range(1, test_loader.dataset.num_spatialGroup + 1):
             for (feat, pid, spaGrpID) in test_loader:
                 l = pid.shape[0]
+                spaGrpID = np.unique(spaGrpID)
                 data, target = feat.cuda(), pid.cuda()
                 data = data.unsqueeze(0).expand(l, l, 262) - data.unsqueeze(1).expand(l, l, 262)
                 target = (target.unsqueeze(0).expand(l, l) - target.unsqueeze(1).expand(l, l)) == 0
                 data, target = data.view(-1, 262).float(), target.view(-1, 1).float()
                 output = model(data)
+                line = np.hstack((spaGrpID * np.ones(output.shape), output.cpu().numpy()))
+                lines = np.vstack((lines, line))
                 test_loss += criterion(output, target).item()  # sum up batch loss
                 pred = output > 0.5  # get the index of the max log-probability
                 correct += pred.eq(target.view_as(pred).byte()).sum().item()
                 miss += target.shape[0] - pred.eq(target.view_as(pred).byte()).sum().item()
                 pass
-            test_loss /= epoch
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, correct + miss, 100. * correct / (correct + miss)))
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.
+                      format(test_loss, correct, correct + miss, 100. * correct / (correct + miss)))
+
+    if args.save_result:
+        output_fname = osp.dirname(args.data_dir) + '/pairwise_dis_%d.h5' % iCam
+        with h5py.File(output_fname, 'w') as f:
+            mat_data = np.vstack(lines)
+            f.create_dataset('dis', data=mat_data, dtype=float)
+            pass
 
 
 def main():
@@ -95,7 +109,10 @@ def main():
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                         help='SGD momentum (default: 0.5)')
     parser.add_argument('--train', action='store_true')
+    parser.add_argument('--save_result', action='store_true')
     # parser.add_argument('--resume', type=str, default='', metavar='PATH')
+    parser.add_argument('--data-dir', type=str, default='~/Data/DukeMTMC/ground_truth/hyperGT_trainval_mini.h5',
+                        metavar='PATH')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=100, metavar='N',
@@ -104,13 +121,13 @@ def main():
 
     torch.manual_seed(args.seed)
 
-    dataset = HyperFeat('../data')
+    dataset = HyperFeat(args.data_dir)
     train_loader = DataLoader(dataset, batch_size=args.batch_size,
                               sampler=RandomIdentitySampler(dataset, args.num_instances),
                               num_workers=0, pin_memory=True)
 
     test_loader = DataLoader(dataset, batch_size=args.test_batch_size,
-                             sampler=RandomIdentitySampler(dataset, 100),
+                             sampler=RandomIdentitySampler(dataset, 10000),
                              num_workers=0, pin_memory=True)
 
     model = Net(num_class=1)
