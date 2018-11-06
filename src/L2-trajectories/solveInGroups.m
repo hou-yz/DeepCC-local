e569935f2655f9ab35c458523b2c8cd896516c53
Yunzhong Hou <hou_yz@outlook.com>
2018-11-07 02:44:10 +0800

    Updates: indifference improves by 0.4 SCT/MCT

--- src/L2-trajectories/solveInGroups.m
+++ src/L2-trajectories/solveInGroups.m
@@ -61,15 +61,12 @@ for i = 1 : length(allGroups)
     if params.compute_score
         % compute appearance and spacetime scores
         appearanceAffinity = getAppearanceMatrix(featureVectors(indices),featureVectors(indices), threshold, diff_p,diff_n,params.step);
-        
+        % compute the correlation matrix
+        correlationMatrix = appearanceAffinity + params.alpha*(spacetimeAffinity-1);
     else
-        appearanceAffinity = getHyperScore(featureVectors(indices),tracklets(indices),hyper_score_param,params.alpha);
-        
+        correlationMatrix = getHyperScore(featureVectors(indices),tracklets(indices),hyper_score_param,params.alpha);
     end
-    % compute the correlation matrix
-    correlationMatrix = appearanceAffinity + params.alpha*(spacetimeAffinity-1);
     correlationMatrix = correlationMatrix .* indifferenceMatrix;
-    
     correlationMatrix(impossibilityMatrix == 1) = -inf;
     correlationMatrix(sameLabels) = 1;
     
