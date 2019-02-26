3dab70dbb0e4aad343824fbb8ddab872198d8b44
hou-yz <hou_yz@ooutlook.com>
2019-02-26 15:02:56 +0800

    Bug fixes for eval

--- src/test_tracker.m
+++ src/test_tracker.m
@@ -5,12 +5,12 @@ function [DukeSCT, DukeMCT] = test_tracker(opts,compute_L1,compute_L2,compute_L3
     if compute_L2
         compute_L2_trajectories(opts);
         opts.eval_dir = 'L2-trajectories';
-        [allMets, metsBenchmark, metsMultiCam] = evaluate(opts);
+        [allMets, metsBenchmark, metsMultiCam] = evaluate(opts,0);
     end
     if compute_L3
         compute_L3_identities(opts);
         opts.eval_dir = 'L3-identities';
-        [allMets, metsBenchmark, metsMultiCam] = evaluate(opts);
+        [allMets, metsBenchmark, metsMultiCam] = evaluate(opts,0);
     end
     DukeSCT = metsBenchmark(1:3);
     DukeMCT = metsMultiCam;
